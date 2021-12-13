8f9efa196ace71c9b0eb3143dc0d3d857c736392
mannanej <mannanej@rose-hulman.edu>
2021-12-13 15:44:18 -0500

    Created Repo and starter file

---
+++ JuniorDesignRobot.ino
@@ -0,0 +1,16 @@
+/*
+ * This is the robot code written by Team Too for ECE362-01 in the Winter of 2021-22. Eddie Mannan, Edward Huang, Kurtis VonBargen, Raymond Zhao and Zackery Painter.
+ * General Info on project, what it does, and how it works
+ * 
+ * Project Started on: 12/13/2021
+ * Project Completed on: TBD
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
